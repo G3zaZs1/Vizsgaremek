d59427b8e156f29a49aabbd94090c87d9ecc9700
G3zaZs1 <120452143+G3zaZs1@users.noreply.github.com>
2025-01-27 13:02:50 +0100

    update-v2-video

---
+++ vizsga3/mainPHP/header.php
@@ -0,0 +1,13 @@
+<header>
+        <div id="mainpage" class="headline">
+            <ul>
+                <li><h3>Main</h3></li>
+                <li><a href="">yes</a></li>
+                <li><a href="download.php">Download</a></li>
+                <li><a href="">maybe</a></li>
+            </ul>
+            <ul id="log">
+                <li><a href="">Logout</a></li>
+            </ul>
+        </div>
+    </header>
---
+++ vizsga3/mainPHP/header2.php
@@ -0,0 +1,14 @@
+<header>
+            <div id="mainpage" class="headline">
+                <ul>
+                    <li><h3>Download</h3></li>
+                    <li><a href="main.php">Main</a></li>
+                    <li><a href="">no</a></li>
+                    <li><a href="">maybe</a></li>
+                </ul>
+                <ul id="log">
+                    <li><a href="">Logout</a></li>
+                </ul>
+            </div>
+
+</header>
